1376895e1b654ff95d88500d05f30eeb293fdd8e
sjaguilar <sergiojavieraguilar@gmail.com>
2020-12-13 08:55:29 -0800

    Adds Bootstrap to app.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,37 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+//
+//
+// import "bootstrap"
+// import "../stylesheets/application"
+//
+//
+// document.addEventListener("turbolinks:load", () => {
+//   $('[data-toggle="tooltip"]').tooltip()
+//   $('[data-toggle="popover"]').popover()
+// }
+//
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import "bootstrap"
+import "../stylesheets/application"
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+  $('.toast').toast({ autohide: false })
+  $('#toast').toast('show')
+})
